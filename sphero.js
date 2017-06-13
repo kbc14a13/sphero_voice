ae1d7a8057f2c30224bead77f695161ac2748a28
yuuki miyata <kbc14a13@stu.kawahara.ac.jp>
2017-06-13 12:48:46 +0900

    sphero.jsの更新と新規ファイル作成 sphero.js内で一部コードに変更。新規に開発用のメモを追加

--- sphero.js
+++ sphero.js
@@ -1,27 +1,16 @@
   "use strict"
 
 var sphero = require("sphero");
-var orb = sphero("COM5");
+var orb = sphero("COM6");
 
 orb.connect(function() {
   // roll Sphero forward
-  orb.roll(150, 0);
-
+  orb.roll(150, 180);
   // turn Sphero green
+  setTimeout(function(){
+    orb.roll(0,180)
+  },10000);
   orb.color("green");
-
   // have Sphero tell you when it detect collisions
-  orb.detectCollisions();
-
-  // when Sphero detects a collision, turn red for a second, then back to green
-  orb.on("collision", function(data) {
-    console.log("collision detected");
-    console.log("  data:", data);
-
-    orb.color("red");
-
-    setTimeout(function() {
-      orb.color("green");
-    }, 100);
-  });
+  
 });
