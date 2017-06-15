a9d5360adf4a6629cfbdf61dde1690fbe4ffc941
yuuki miyata <kbc14a13@stu.kawahara.ac.jp>
2017-06-15 12:25:36 +0900

    sphero.jsの修正 前回のバージョンだと正常にorb.rollが動かないため引数にorbを追加

--- sphero.js
+++ sphero.js
@@ -6,25 +6,25 @@ var sphero = require("sphero");
 var orb = sphero("COM6");
 */
 //引数は角度
-var contol = function(rote){
+var contol = function(orb,rote){
     orb.roll(150,rote);
-            setTimeout(function(){
-                orb.roll(0,360-rote);
-            },10000);
+        setTimeout(function(){
+            orb.roll(0,360-rote);
+        },10000);
 };
 
-var move_left = function(){
-    contol(90);
+var move_left = function(orb){
+    contol(orb,90);
 };
 
-var move_right = function(){
-    contol(270);
+var move_right = function(orb){
+    contol(orb,270);
 };
 
-var move_advance = function(){
-    contol(0);
+var move_advance = function(orb){
+    contol(orb,0);
 };
 
-var move_back = function(){
-    contol(180);
+var move_back = function(orb){
+    contol(orb,180);
 };
