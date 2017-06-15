73ce5c657bf32979e9ce399760c3ba46e7604ab3
yuuki miyata <kbc14a13@stu.kawahara.ac.jp>
2017-06-15 11:48:49 +0900

    sphero.jsの編集 呼び出し用の関数の作成

--- sphero.js
+++ sphero.js
@@ -1,16 +1,30 @@
-  "use strict"
-
+//これを使用する際は以下のコメントを解除してコントローラー側に記入してください
+/*
+//sphero等の種類を指定
 var sphero = require("sphero");
+//Bluetoothの特定のspheroに対して指定されてる送信portを入力
 var orb = sphero("COM6");
+*/
+//引数は角度
+var contol = function(rote){
+    orb.roll(150,rote);
+            setTimeout(function(){
+                orb.roll(0,360-rote);
+            },10000);
+};
 
-orb.connect(function() {
-  // roll Sphero forward
-  orb.roll(150, 180);
-  // turn Sphero green
-  setTimeout(function(){
-    orb.roll(0,180)
-  },10000);
-  orb.color("green");
-  // have Sphero tell you when it detect collisions
-  
-});
+var move_left = function(){
+    contol(90)
+};
+
+var move_right = function(){
+    contol(270)
+};
+
+var move_advance = function(){
+    contol(0)
+};
+
+var move_back = function(){
+    contol(180)
+};
