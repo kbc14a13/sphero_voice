80f1becb7dae27d9c399a43bc92456378b8f7ce0
yuuki miyata <kbc14a13@stu.kawahara.ac.jp>
2017-06-22 11:08:02 +0900

    varをconstに変更 close #3

--- sphero.js
+++ sphero.js
@@ -1,40 +1,40 @@
 //これを使用する際は以下のコメントを解除してコントローラー側に記入してください
 /*
 //sphero.jsの呼び出し
-var connecr =require("./sphero.js")
+const connecr =require("./sphero.js")
 //sphero等の種類を指定
-var sphero = require("sphero");
+const sphero = require("sphero");
 //Bluetoothの特定のspheroに対して指定されてる送信portを入力
-var orb = sphero("COM6");
+const orb = sphero("COM6");
 */
 //引数は角度
 
-var sphero = require("sphero");
+const sphero = require("sphero");
 
-var control = function(orb,rote){
+const control = function(orb,rote){
     orb.roll(150,rote);
         setTimeout(function(){
             orb.roll(0,360-rote);
         },10000);
 };
 
-var move_left = function(orb){
+const move_left = function(orb){
     control(orb,270);
 };
 
-var move_right = function(orb){
+const move_right = function(orb){
     control(orb,90);
 };
 
-var move_advance = function(orb){
+const move_advance = function(orb){
     control(orb,0);
 };
 
-var move_back = function(orb){
+const move_back = function(orb){
     control(orb,180);
 };
 
-var move_stop = function(orb){
+const move_stop = function(orb){
     orb.roll.bind(orb,0,0);
 };
 
