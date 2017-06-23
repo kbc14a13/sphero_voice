b019ebc41b38fb0b90eaf16f016e0e7212768f0d
yuuki miyata <kbc14a13@stu.kawahara.ac.jp>
2017-06-23 11:58:06 +0900

    ポートをキーで指定する機能の実装 close #2

--- spheroChoice.js
+++ spheroChoice.js
@@ -1,15 +1,14 @@
 const choiceJson = require("./config.json");
 
 const keypress = require('keypress');
-console.log("2")
 const choice = function(callback){
     // make `process.stdin` begin emitting "keypress" events 
     keypress(process.stdin);
-    console.log("3")
+    console.log("F1:RPO");
+    console.log("F2:PBW");
     
     // listen for the "keypress" event 
     process.stdin.on('keypress', function (ch, key) {
-        console.log(choiceJson.PRO);
         if (key.ctrl && key.name == 'c') {
             process.stdin.pause();
         }else if (key.name == 'f1') {
