c3d56f65e2bd2b5c2cd31fde2cb16670139ff360
Huruikagi <kbc14a04@stu.kawahara.ac.jp>
2017-06-27 14:10:28 +0900

    speroChoiceのリファクタリング コメントの追加および、if-elseの羅列を整理

--- spheroChoice.js
+++ spheroChoice.js
@@ -1,35 +1,69 @@
 const choiceJson = require("./config.json");
-
 const keypress = require('keypress');
+
+
+/**
+ * config.jsonに記述されている設定情報と照らしあわせて、
+ * キーボード入力によって接続先のSpheroを選ぶために利用する関数です。
+ * 
+ * @param {(portName: string) => void} callback 
+ */
 const choice = function(callback){
-    // make `process.stdin` begin emitting "keypress" events 
+
+    // 標準入力がkeypressイベントを発火できるようにする。
     keypress(process.stdin);
+
+    // F1かF2を押すように促す表示をする。
     console.log("F1:RPO");
     console.log("F2:PBW");
-    var choiceDecison = false;
-    // listen for the "keypress" event 
+
+    // 選択が完了したかどうかを表すフラグ
+    // 完了していたらtrue。
+    let choiceDecison = false;
+
+    // keypressイベントを監視
     process.stdin.on('keypress', function (ch, key) {
+
         if (key.ctrl && key.name == 'c') {
+            // Ctrl+Cが押された時はキャンセル
             process.stdin.pause();
-        }else if (key.name == 'f1'&&choiceDecison == false) {
-            process.stdin.pause();
-            choiceDecison = true;
-            callback(choiceJson.RPO);
-        }else if (key.name == 'f2'&&choiceDecison == false) {
-            process.stdin.pause();
-            choiceDecison = true;
-            callback(choiceJson.PBW);
-        }else if (choiceDecison == false){
-            console.log("指定されたキー以外のキーが押されました");
-            console.log("F1:RPO");
-            console.log("F2:PBW");
+
+        } else if (!choiceDecison) {
+            // ポートが未選択の間だけ動作する
+            switch(key.name) {
+
+                // f1かf2が押された時は、コールバック関数経由で
+                // 選択されたポートを表す文字列を外部に通知します。
+                case 'f1':
+                    process.stdin.pause();
+                    choiceDecison = true;
+                    callback(choiceJson.RPO);
+                    break;
+
+                case 'f2':
+                    process.stdin.pause();
+                    choiceDecison = true;
+                    callback(choiceJson.PBW);
+                    break;
+
+                default:
+                    // 未定義のキーが押された時は、再度入力を促す。
+                    console.log("指定されたキー以外のキーが押されました");
+                    console.log("F1:RPO");
+                    console.log("F2:PBW");
+            }
+
         }
     });
  
+    // 標準入力のストリームを入力受付状態にする。
+    // process.stdin.pause()が呼び出されるまでそのままになる。
     process.stdin.setRawMode(true);
     process.stdin.resume();
 };
 
+
+// 必要となるモジュールのエクスポート
 module.exports = {
     choice: choice
 };
